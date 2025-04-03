1e06ab4de33642341962f2865565abbb8fbefe0e
Whiteflakes <77163955+Whiteflakes@users.noreply.github.com>
2025-04-03 14:01:09 +0530

    Enhance project structure and add CI workflows for testing and publishing

--- setup.py
+++ setup.py
@@ -3,10 +3,17 @@ from setuptools import setup, find_packages
 setup(
     name="dircraft",
     version="0.1.0",
+    author="Whiteflakes",
+    author_email="whiteflakesdev@gmail.com",
+    description="A tool to generate project directory structures from specifications.",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Whiteflakes/dircraft", 
     packages=find_packages(),
+    include_package_data=True,
     install_requires=[
-        "PyYAML",
-        "Click",
+        "pyyaml",
+        "click",
     ],
     entry_points={
         "console_scripts": [
@@ -20,8 +27,4 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
-    author="Whiteflakes",
-    description="A tool to generate file structures from YAML configurations.",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",  
 )
