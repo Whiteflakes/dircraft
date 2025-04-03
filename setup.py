645547b1f3e5d4de0db0512bf8d8b6ab43e8eb50
Whiteflakes <77163955+Whiteflakes@users.noreply.github.com>
2025-04-03 13:19:54 +0530

    Updates name as well as structure

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="scaff",
+    name="dircraft",
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
@@ -10,7 +10,8 @@ setup(
     ],
     entry_points={
         "console_scripts": [
-            "scaff = scaff.cli:main",
+            "dircraft = dircraft.cli:main",
+            "dircraft-gui = dircraft.gui:main",
         ],
     },
     classifiers=[
